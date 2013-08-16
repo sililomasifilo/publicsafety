91a8b49cb5e9b46594d087bcd85f52526c2d49ed
Anurag Ramdasan <anuragr@live.com>
2013-08-16 17:02:47 +0530

    Update database.php

--- src/server/laravel/app/config/database.php
+++ src/server/laravel/app/config/database.php
@@ -57,7 +57,7 @@ return array(
 			'host'      => 'localhost',
 			'database'  => 'publicsafety',
 			'username'  => 'root',
-			'password'  => 'rootAnurag1112',
+			'password'  => 'root',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
